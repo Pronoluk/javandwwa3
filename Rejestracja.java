bcb159a86a942b13c52312a15b2353cf321f20a2
Lukasz Pronobis <pronoluk@gmail.com>
2018-03-10 12:47:13 +0100

    branch Logowanie

--- Rejestracja.java
+++ Rejestracja.java
@@ -1,3 +1,3 @@
-public class Rejestracja{
+ public class rejestracja {
 
-}
+ }
--- logowanie.java
+++ logowanie.java
@@ -1,6 +1,7 @@
-public class{
+public class {
 
     private int i=10;
     private int j=15;
+    private int k=20;
 
 }
